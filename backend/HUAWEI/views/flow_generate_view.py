31a6020522819111a09c46fb34e78dae3aad6611
Fan Ruwen <fruwen@163.com>
2021-04-13 18:49:26 +0800

    changed flow models & timestamp,#57

--- backend/HUAWEI/views/flow_generate_view.py
+++ backend/HUAWEI/views/flow_generate_view.py
@@ -4,36 +4,26 @@ from rest_framework.views import APIView
 from HUAWEI.models import Site, Equipment, RawFlowData
 import random, json, time
 
-def generate_flow():
-    queryset_flow = RawFlowData.objects.all()
-    nums = 100000
-    up = 0
-    down = 0
-    newnums = random.randint(0, 100)
-    for i in range(0, newnums):
-        k = random.randint(1, nums + 1)
-        new_flow = queryset_flow.get(pk=k)
-        up += new_flow.out_flow
-        down += new_flow.in_flow
-    return {'up': up, 'down': down}
-
 
 class FlowGenerateView(APIView):
     permission_classes = [AllowAny]
 
     queryset_site = Site.objects.all()
     queryset_eq = Equipment.objects.all()
+    queryset_flow = RawFlowData.objects.all()
+    flow_nums = len(queryset_eq)
     def get(self, request):
-        local_time = time.localtime()
-        site_time = str(local_time.tm_year) + '-' + str(local_time.tm_mon) + '-' + str(local_time.tm_mday) + ' ' \
-                    + str(local_time.tm_hour) + ':' + str(local_time.tm_min) + ':' + str(local_time.tm_sec)
+        time_array = time.localtime()
+        site_time = time.strftime("%Y-%m-%d %H:%M:%S", time_array)
         for i in self.queryset_site:
+            if not i.status == 0:
+                continue
             try:
                 flow_list = json.loads(i.flow_data)
             except:
                 flow_list = []
             if len(flow_list) >= 6:
-                del(flow_list[0])
+                del (flow_list[0])
             eqset = self.queryset_eq.filter(site=i.pk)
             site_up = 0
             site_down = 0
@@ -43,8 +33,8 @@ class FlowGenerateView(APIView):
                 except:
                     flow_list_eq = []
                 if len(flow_list_eq) >= 6:
-                    del(flow_list_eq[0])
-                new_flow = generate_flow()
+                    del (flow_list_eq[0])
+                new_flow = self.generate_flow()
                 new_flow['time'] = site_time
                 flow_list_eq.append(new_flow)
                 j.total_up += new_flow['up']
@@ -59,3 +49,14 @@ class FlowGenerateView(APIView):
             i.flow_data = json.dumps(flow_list)
             i.save()
         return Response("", 200)
+
+    def generate_flow(self):
+        up = 0
+        down = 0
+        newnums = random.randint(0, 100)
+        for i in range(0, newnums):
+            k = random.randint(1, self.flow_nums)
+            new_flow = self.queryset_flow.get(pk=k)
+            up += new_flow.out_flow
+            down += new_flow.in_flow
+        return {'up': up, 'down': down}
