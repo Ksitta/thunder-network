98d54e0b03be2465b5e1e1abf4873c0bcafea15c
Fan Ruwen <fruwen@163.com>
2021-04-13 20:31:24 +0800

    remove timmer, #59

--- backend/HUAWEI/views/__init__.py
+++ backend/HUAWEI/views/__init__.py
@@ -1,24 +1,24 @@
-from threading import Timer
-import time
-from .flow_generate_view import FlowGenerateView
-try:
-    from config.local_settings import deploy
-except:
-    deploy = False
-
-generator = FlowGenerateView()
-
-global t
-
-def get_flow():
-    global t
-    logfile = open('./flow.log', mode='w+')
-    logfile.write(str(time.time()) + " generated a flow message")
-    logfile.close()
-    generator.inner_generate()
-    t = Timer(14400, get_flow)
-    t.start()
-
-if deploy:
-    t = Timer(14400, get_flow)
-    t.start()
+# from threading import Timer
+# import time
+# from .flow_generate_view import FlowGenerateView
+# try:
+#     from config.local_settings import deploy
+# except:
+#     deploy = False
+#
+# generator = FlowGenerateView()
+#
+# global t
+#
+# def get_flow():
+#     global t
+#     logfile = open('./flow.log', mode='w+')
+#     logfile.write(str(time.time()) + " generated a flow message")
+#     logfile.close()
+#     generator.inner_generate()
+#     t = Timer(14400, get_flow)
+#     t.start()
+#
+# if deploy:
+#     t = Timer(14400, get_flow)
+#     t.start()
