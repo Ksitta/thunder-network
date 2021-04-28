071ce7037ee80703107724a71d76a31c6458e578
Fan Ruwen <fruwen@163.com>
2021-04-28 21:56:06 +0800

    fixed bug of timmer, #70

---
+++ backend/HUAWEI/task.py
@@ -0,0 +1,41 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from django.test.client import Client
+from django.urls import reverse
+from HUAWEI.models import Site, Equipment, RawFlowData, FlowData
+import time, random
+
+
+
+def generate_flow(user, site, eq, now_time, create_list, queryset_flow, flow_nums):
+    new_nums = random.randint(0, 100)
+    for i in range(0, new_nums):
+        k = random.randint(1, flow_nums)
+        raw_flow = queryset_flow.get(pk=k)
+        new_flow = FlowData(dest_ip=raw_flow.dest_ip, source_ip=raw_flow.source_ip,
+                            out_flow=raw_flow.out_flow, in_flow=raw_flow.in_flow,
+                            site=site, user=user, eq=eq, generate_time=now_time)
+        create_list.append(new_flow)
+
+def flow_info():
+    queryset_site = Site.objects.all()
+    queryset_flow = RawFlowData.objects.all()
+    queryset_eq = Equipment.objects.all()
+    flow_nums = len(queryset_flow)
+    with open('./flow.log', mode='a+') as file:
+        file.write(str(time.time()) + " generated flow info\n")
+    now_time = time.time()
+    create_list = []
+    for site in queryset_site:
+        if not site.status == 0:
+            continue
+        eq_set = queryset_eq.filter(site=site.pk)
+        for eq in eq_set:
+            if not eq.eq_status == 1:
+                continue
+            generate_flow(site.user, site, eq, now_time, create_list, queryset_flow, flow_nums)
+    FlowData.objects.bulk_create(create_list)
+
+
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(flow_info, 'interval', seconds=60)
+# scheduler.start()
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,7 +44,24 @@ INSTALLED_APPS = [
     'HUAWEI.apps.HuaweiConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_q',
 ]
+
+Q_CLUSTER = {
+    'name': 'myproject',#项目名称
+   'workers': 4,  #worker数。默认为当前主机的CPU计数，
+    'recycle': 500,  # worker在回收之前要处理的任务数。有助于定期释放内存资源。默认为500。
+    'timeout': 60,  # 任务超时设置,如果是爬虫任务建议设置长一些
+    'compress': True,  # 数据压缩
+    'save_limit': 250,  # 限制保存到Django的成功任务的数量。0为无限，-1则不会保存
+    'queue_limit': 500,  # 排队的任务数量，默认为workers**2。
+    'cpu_affinity': 1,  # 设置每个工作人员可以使用的处理器数量。根据经验; cpu_affinity 1支持重复的短期运行任务，而没有亲和力则有利于长时间运行的任务。
+     'label': 'Django Q',  # 用于Django Admin页面的标签。默认为'Django Q'，之后我会根据源码做一个中文版的django-admin页面。如果有需求请私信我
+    'redis': {#如果配置了redis缓存，可以使用django的设置，请参考官方文档。
+        'host': '127.0.0.1',
+        'port': 6379,
+        'db': 0, }
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
