fe2509c0d9d0a3cc3e7c174025bb9000a6265e19
Fan Ruwen <fruwen@163.com>
2021-04-29 10:10:38 +0800

    fixed bug of timer 2, #76

--- backend/HUAWEI/views/__init__.py
+++ backend/HUAWEI/views/__init__.py
@@ -3,7 +3,6 @@ from .cron import flow_info, test_con
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(flow_info, 'interval', seconds=3600)
-scheduler.start()
 
 scheduler.add_job(test_con, 'interval', seconds=60)
 scheduler.start()
