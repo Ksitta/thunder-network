79eebd29ee6f273cd77b30d42ece115b5563068d
Fan Ruwen <fruwen@163.com>
2021-04-29 12:05:02 +0800

    fixed timer bug, #76

--- backend/HUAWEI/views/__init__.py
+++ backend/HUAWEI/views/__init__.py
@@ -1,8 +1,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .flow_generate_view import FlowGenerateView
+try:
+    from config.local_settings import interval_time
+except:
+    interval_time = 3600
 
 a = FlowGenerateView()
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(a.inner_generate, 'interval', seconds=60)
+scheduler.add_job(a.inner_generate, 'interval', seconds=interval_time)
 scheduler.start()
