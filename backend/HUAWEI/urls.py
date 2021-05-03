dfe93445bfabd96e434aa04e230b953684cb0f79
Fan Ruwen <fruwen@163.com>
2021-05-04 00:58:53 +0800

    added mail function, #86

--- backend/HUAWEI/urls.py
+++ backend/HUAWEI/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.conf.urls import url
-from .views import site_views, user_views, eq_views, flow_views, assign_views, flow_generate_view, ssid_views
+from .views import site_views, user_views, eq_views, flow_views, assign_views, flow_generate_view, ssid_views, mail_views
 
 ROUTER = DefaultRouter()
 ROUTER.register('user/register', user_views.UserViewSet, basename='user')
@@ -12,6 +12,7 @@ urlpatterns = [
     url('^user/edit/$', user_views.UserProfileView.as_view(), name='edit'),
     url('^user/token/$', user_views.TokenObtainView.as_view(), name='token'),
     url('^user/refresh/$', TokenRefreshView.as_view(), name='refresh'),
+    url('^mail/$', mail_views.MailSendView.as_view(), name='mail'),
     url(r'^site/$', site_views.SiteView.as_view(), name='site'),
     url(r'^site/(?P<pk>[0-9]+)/$', site_views.SiteDetailView.as_view(), name='site_detail'),
     url(r'^assign/(?P<pk>[0-9]+)/$', assign_views.AssignView.as_view(), name='assign'),
--- backend/HUAWEI/views/mail_views.py
+++ backend/HUAWEI/views/mail_views.py
@@ -1,4 +1,14 @@
 from django.core.mail import send_mail
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
 # send_mail的参数分别是  邮件标题，邮件内容，发件箱(settings.py中设置过的那个)，收件箱列表(可以发送给多个人),失败静默(若发送失败，报错提示我们)
-send_mail('Subject here', 'Here is the message.', 'thunder_network@126.com',
-    ['zhang-qh19@mails.tsinghua.edu.cn'], fail_silently=False)
+
+class MailSendView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        send_mail('Subject here', 'Here is the message.', 'thunder_network@126.com',
+                  ['xxx@xxx.com'], fail_silently=False)
+        return Response("Success", 200)
