071194f85ba9b762c07c512518ca6d989ee853f9
Fan Ruwen <fruwen@163.com>
2021-05-07 20:45:44 +0800

    added more info of email, #91

--- backend/HUAWEI/views/mail_views.py
+++ backend/HUAWEI/views/mail_views.py
@@ -17,13 +17,29 @@ def trans_time(timestamp):
     utc_loc_time = utc.localize(utc_date)
     return utc_loc_time.astimezone(beijing).strftime(fmt)
 
-def send_email(user_name, email_address):
+def trans_cacul(a:int):
+    if a == 1:
+        return 0.8
+    else:
+        return 0.7
+
+def send_email(user_name, email_address, mory):
+    if mory == 1:
+        time_dec = 2626560
+        time_p = '月度'
+    else:
+        time_dec = 31536000
+        time_p = '年度'
     from_email = "thunder_network@126.com"
     user = User.objects.get(username=user_name)
-    start_time = int(time.time()) - 2626560
+    start_time = int(time.time()) - time_dec
     site = Site.objects.filter(user=user.pk)
     flow_list = []
     total_flow = 0
+    total_money_num = 0.0
+    month = 0
+    year = 0
+    time_now = time.time()
     for i in site:
         site_flow = FlowData.objects.filter(site=i, generate_time__gte=start_time)
         one_site_flow = 0
@@ -31,9 +47,22 @@ def send_email(user_name, email_address):
             one_site_flow += each.in_flow + each.out_flow
         one_site_flow /= (1024 * 1024 * 1024)
         total_flow += one_site_flow
+        site_money = one_site_flow * trans_cacul(i.billing_level)
         flow_list.append({'site_name': i.site_name, 'site_flow': '%.2f' % one_site_flow,
-                          'site_money': '%.2f' % (one_site_flow * 0.8)})
-    total_money = '%.2f' % (total_flow * 0.8)
+                          'site_money': '%.2f' % site_money})
+        total_money_num += site_money
+        if i.billing_level == 1:
+            month += 1
+            if mory == 2:
+                month + (time_now - i.create_time.timestamp()) / 2626560
+        else:
+            year += 1
+    if mory == 1:
+        flow_list.append({'site_name': '基础月费(不包含年费订单)', 'site_money': month * 10})
+    else:
+        flow_list.append({'site_name': '基础月费(包含年费和月费)', 'site_money': month * 10 + year * 50})
+    total_money_num += month * 10
+    total_money = '%.2f' % total_money_num
     if email_address:
         to_list = [
             email_address
@@ -47,7 +76,7 @@ def send_email(user_name, email_address):
     html_body = render_to_string('mail.html', mail_data)
 
     text_content = ''  # 对方不支持多媒体邮件的话显示这里的内容
-    subject = '您的本月账单，请您查收 Thunder-network'  # 邮件标题
+    subject = '您的本'+time_p+'的账单，请您查收 Thunder-network'  # 邮件标题
     msg = EmailMultiAlternatives(subject, text_content, from_email, to_list)
     # 邮件显示html_body的内容，html编码
     msg.attach_alternative(html_body, "text/html")
@@ -60,7 +89,9 @@ class MailSendView(APIView):
         data = request.data
         try:
             email_address = data['email']
+            username = data["username"]
         except:
             email_address = None
-        send_email(data["username"], email_address)
+            username = self.request.user
+        send_email(username, email_address, data['mory'])
         return Response("Success", 200)
